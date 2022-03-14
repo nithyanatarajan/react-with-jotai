404a24f18e8822c7bb85a8b87abaef398adee1ca
Nithya Natarajan <nithyanatarajn@gmail.com>
2022-03-14 13:06:21 +0530

    Create a writeonly atom to avoid rerendering
    TLDR - Avoid write and read of same atom in a component

--- src/draw/components/SvgRoot.js
+++ src/draw/components/SvgRoot.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { useAtomValue } from 'jotai';
 import { useUpdateAtom } from 'jotai/utils';
 import { useCommitCount } from '../hooks';
-import { dotsAtom, drawingAtom } from '../state';
+import { dotsAtom, drawingAtom, updateDotsAtom } from '../state';
 
 const SvgDots = () => {
   const dots = useAtomValue(dotsAtom);
@@ -16,26 +16,8 @@ const SvgDots = () => {
 };
 
 const SvgRoot = () => {
-  const setDotsAtom = useUpdateAtom(dotsAtom);
+  const addPoint = useUpdateAtom(updateDotsAtom);
   const setDrawingAtom = useUpdateAtom(drawingAtom);
-  const drawing = useAtomValue(drawingAtom);
-  const addPoint = (point) => {
-    // The component is re-rendered every time there is a mouse down event
-    // as we read from drawing atom
-    if (drawing) { setDotsAtom((prev) => [...prev, point]); }
-  };
-  /*
-  The setDotsAtom and addPoint are same as the following write only atom
-  atom(
-    null,
-    (get, set, update) => {
-     if (get(drawingAtom)) {
-      set(dotsAtom, (prev) => [...prev, update]);
-     }
-    },
-  );
-  */
-
   const handleMouseUp = () => setDrawingAtom(false);
   const handleMouseDown = () => setDrawingAtom(true);
 
--- src/draw/state/index.js
+++ src/draw/state/index.js
@@ -4,4 +4,13 @@ export const dotsAtom = atom([]);
 export const dotsLengthAtom = atom((get) => get(dotsAtom).length);
 export const drawingAtom = atom(false);
 
+export const updateDotsAtom = atom(
+  null,
+  (get, set, update) => {
+    if (get(drawingAtom)) {
+      set(dotsAtom, (prev) => [...prev, update]);
+    }
+  },
+);
+
 export default {};
