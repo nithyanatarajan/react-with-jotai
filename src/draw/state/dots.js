cf26cdebadae8b2c08097a2ddbef1c3258521cfb
Nithya Natarajan <nithyanatarajn@gmail.com>
2022-03-15 00:35:29 +0530

    Add addDotAtom
    Description: create a writable atom from an writable atom

--- src/draw/state/dots.js
+++ src/draw/state/dots.js
@@ -4,11 +4,18 @@ import { drawingAtom } from './drawing';
 export const dotsAtom = atom([]);
 export const dotsLengthAtom = atom((get) => get(dotsAtom).length);
 
+const addDotAtom = atom(
+  null,
+  (_get, set, update) => {
+    set(dotsAtom, (prev) => [...prev, update]);
+  },
+);
+
 export const updateDotsonDrawingAtom = atom(
   null,
   (get, set, update) => {
     if (get(drawingAtom)) {
-      set(dotsAtom, (prev) => [...prev, update]);
+      set(addDotAtom, update);
     }
   },
 );
