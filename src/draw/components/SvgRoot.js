c1d40f6d6ed12b207aff54c34352c1823e45cfcf
Nithya Natarajan <nithyanatarajn@gmail.com>
2022-03-14 13:02:33 +0530

    Add mouse down event to draw components

--- src/draw/components/SvgRoot.js
+++ src/draw/components/SvgRoot.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { useAtomValue } from 'jotai';
 import { useUpdateAtom } from 'jotai/utils';
 import { useCommitCount } from '../hooks';
-import { dotsAtom } from '../state';
+import { dotsAtom, drawingAtom } from '../state';
 
 const SvgDots = () => {
   const dots = useAtomValue(dotsAtom);
@@ -17,22 +17,35 @@ const SvgDots = () => {
 
 const SvgRoot = () => {
   const setDotsAtom = useUpdateAtom(dotsAtom);
-  const addPoint = (point) => setDotsAtom((prev) => [...prev, point]);
+  const setDrawingAtom = useUpdateAtom(drawingAtom);
+  const drawing = useAtomValue(drawingAtom);
+  const addPoint = (point) => {
+    // The component is re-rendered every time there is a mouse down event
+    // as we read from drawing atom
+    if (drawing) { setDotsAtom((prev) => [...prev, point]); }
+  };
   /*
-  The above two line are same as the following write only atom
+  The setDotsAtom and addPoint are same as the following write only atom
   atom(
     null,
     (get, set, update) => {
+     if (get(drawingAtom)) {
       set(dotsAtom, (prev) => [...prev, update]);
+     }
     },
   );
   */
+
+  const handleMouseUp = () => setDrawingAtom(false);
+  const handleMouseDown = () => setDrawingAtom(true);
 
   return (
     <svg
       width="200"
       height="200"
       viewBox="0 0 200 200"
+      onMouseUp={handleMouseUp}
+      onMouseDown={handleMouseDown}
       onMouseMove={(e) => addPoint([e.clientX, e.clientY])}
     >
       <rect width="200" height="200" fill="#eee" />
--- src/draw/state/index.js
+++ src/draw/state/index.js
@@ -2,5 +2,6 @@ import { atom } from 'jotai';
 
 export const dotsAtom = atom([]);
 export const dotsLengthAtom = atom((get) => get(dotsAtom).length);
+export const drawingAtom = atom(false);
 
 export default {};
